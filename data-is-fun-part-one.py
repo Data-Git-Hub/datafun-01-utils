bf45a02e2d451b419f2971a54dbb7bb85fe14ccd
Data Analytics Graduate Student <s576977@nwmissouri.edu>
2025-01-13 16:30:16 -0600

    Update data-is-fun-part-one.py
    # ver 1.3 
    - Added Truth Value Testing

--- data-is-fun-part-one.py
+++ data-is-fun-part-one.py
@@ -24,11 +24,23 @@ TODO: Change the module name in this opening docstring
 # Define global functions (reusable instructions)
 #####################################
 
+# Truth Value Testing 
+'''
+constants defined to be false: None and False
+zero of any numeric type: 0, 0.0, 0j, Decimal(0), Fraction(0, 1)
+empty sequences and collections: '', (), [], {}, set(), range(0)
+'''
+'''
+x or y  | if x is true, then x, else y
+x and y | if x is false, then x, else y
+not x   | if x is false, then True, else False
+'''
+
 #####################################
 # Define main() function for this module.
 #####################################
 
-# def main() -> None:
+def main() -> None:
     '''
     Print results of get_byline() when main() is called.
 
@@ -38,11 +50,12 @@ TODO: Change the module name in this opening docstring
     It doesn't need any additional information passed in, 
     so there's nothing inside the parentheses.
     Everything after the colon must be indented consistently (usually four spaces)
-    '''
 
-#   print("Starting........")
-#   print(get_byline())
-#   print("Complete.......")
+    This is used in best practices when coding.
+    '''
+    print("Starting........")
+    print(get_byline())
+    print("Complete.......")
 
 #####################################
 # Conditional Execution
@@ -51,7 +64,7 @@ TODO: Change the module name in this opening docstring
 # If we are running this file as a script, then call main()
 # and verify our code works as expected.
 
-# if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
 
 #TODO: Run this as a script and verify all code works as intended.
